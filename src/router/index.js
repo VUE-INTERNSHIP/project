81796f3c84cae05aff019424ca8d512b01222403
Mfonido <favourmark05@gmail.com>
2022-10-05 12:38:19 +0100

    fixed login

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import login from '../components/Authentication/login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'login',
+    component: login,
   },
   {
     path: '/about',
