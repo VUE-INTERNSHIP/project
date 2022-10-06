1cbf04e9f6678bd26fcbcd20d8e31b25f4ce1fe9
preshpi <egwuenuprecious7@gamil.com>
2022-10-06 13:20:12 +0100

    Added the trending component

---
+++ src/components/Trending.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- This is for the trending component -->
+    <div class="conatiner">
+      Trending
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  .conatiner{
+      border:2px solid black;
+      height:200px;
+  }
+  </style>
