7059e4b4fe1f8eb511026ebbcb83d61bffabf311
Alamieen <aminugarbazainabari@gmail.com>
2022-10-30 18:45:39 +0100

    add responsiveness to recommended component

--- src/components/recomendedComponent.vue
+++ src/components/recomendedComponent.vue
@@ -1,46 +1,57 @@
 <template>
-
   <div>
-   
-    <h2>Recommended for you</h2>
-    <p></p> 
-    <div class="container">
-      <div class="row">
-        <div class="col-4 d-flex" v-for="movie in movies">
-          <div class="movie">
-            <img class="movie-img-top" :src="movie.thumbnail.regular.large" alt="Card image cap">
-            <div class="movie-body">
-              <div class="details">
-                <div class="year">
-                  <p>{{movie.year}} <i class="fa fa-circle" id="circle"></i></p>
-                </div>
-                <div class="category">
-                  <p v-bind="movie">{{movie.category}} <i class="fa-solid fa-film" id="movieIcon"></i><i class="fa fa-circle"
-                      id="circle2"></i></p>
+    <h3>Recommended for you</h3>
+    <div class="recommended">
 
-
-                </div> 
-                <div class="rating">
-                  {{movie.rating}}
-
-                </div>
-
-              </div>
-              <h5 class="movie-title">{{movie.title}}</h5>
-
+      <div v-for="movie in movies" class="#">
+        <div class="movie">
+          <img class="movie-img-top" :src="movie.thumbnail.regular.large" alt="Card image cap" />
+          <div class="booked">
+            <span v-if="movie.isBookmarked" class="book"><img src="../assets/bookMarkFull.svg"
+                :alt="movie.title" /></span>
+            <span v-if="!movie.isBookmarked" class="book"><img src="../assets/icon-bookmark-empty.svg"
+                :alt="movie.title" /></span>
+          </div>
+          <div class="play-container"> kkk
+            <div class="play">
+              <img src="@/assets/icon-play.svg" alt="" srcset="" /><span>Play</span>
             </div>
           </div>
-
-          <div id="images">
-            <!-- <img :src="movie.thumbnail.regular.large" class="card-img-top" alt="product image"> -->
-            {{film}}
+          <div class="movie-body">
+            <div class="details">
+              <div class="year">
+                <p>{{ movie.year }}</p>
+              </div>
+              <div class="list">
+                <ul>
+                  <li>
+                  </li>
+                </ul>
+              </div>
+              <div class="catImg">
+                <span v-if="movie.category === 'TV Series'" class="span"><img class="svg"
+                    src="../assets/icon-category-tv.svg" alt="" /></span> <span v-else class="span"><img class="svg"
+                    src="../assets/icon-category-movie.svg" alt="" /></span>
+              </div>
+              <div class="catName">
+                <p>
+                  {{ movie.category }}
+                </p>
+              </div>
+              <div class="rating">
+                <ul>
+                  <li>
+                    {{ movie.rating }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <h4 class="movie-title">{{ movie.title }}</h4>
           </div>
         </div>
       </div>
     </div>
-
   </div>
-
 </template>
 
 <script>
@@ -48,28 +59,39 @@
 
 //import movies from '@/datas/movies';
 
-
 export default {
-
-  props: ['movies'],
+  props: ["movies"],
 
   data() {
-    return {
-
-    }
-
-  }
-}
+    return {};
+  },
+};
 </script>
 
-
 <style scoped>
+h3 {
+  padding: 1%;
+  color: whitesmoke;
+  margin-left: 4%;
+}
 
-h2{
+h2 {
   text-align: left;
-  
-  margin-top:1%;
-color: whitesmoke;
+
+
+  color: whitesmoke;
+}
+
+.recommended {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  grid-auto-rows: auto;
+  width: 95%;
+  margin: auto;
+  grid-gap: 1rem;
+
+
 }
 
 img {
@@ -77,51 +99,165 @@ img {
   height: 200px;
   width: 400px;
   border-radius: 18px;
-
 }
 
 .details {
   display: flex;
   flex-direction: row;
   margin-top: 1%;
+  gap: 8%;
   color: #d3d3d398;
   text-align: left;
-  margin-left: 0%;
-
+  margin-left: 1%;
 }
 
 
-.category {
-  margin-left: 4%;
-}
-.rating{
+
+.catImg {
+  position: absolute;
   margin-left: 5%;
 }
+
+.catImg img {
+  height: 20px;
+  width: 20px;
+  border-radius: 2px;
+  margin-top: 2px;
+
+}
+
+
+
+
+li {
+  font-size: 14px;
+  font-weight: 100;
+
+
+}
+
+
 .movie-title {
-  margin-top: -5%;
-  margin-left: 0;
+  position: absolute;
+  margin-left: 0%;
   text-align: left;
-  font-weight: 400;
-  color: aliceblue;
-  padding-top: 2%;
+  font-weight: 500;
+  color: whitesmoke;
+  padding-bottom: 15px;
+  margin-top: px;
 }
 
-/*#movieIcon{
+.span {
+  display: flex;
+  align-items: center;
+}
 
-}*/
-#circle {
-  font-size: 4px;
-  margin-top: 10px;
-  margin-left: 1%;
+.booked {
   position: absolute;
-  color: #d3d3d398;
+  margin-left: 25%;
+  margin-top: -15%;
+  background: rgba(0, 0, 0, 0.4);
+  padding: 10px;
+  border-radius: 50%;
+  cursor: pointer;
 }
-#circle2{
-  font-size: 4px;
-  margin-top: 10px; 
-  position: absolute;
-  color: #d3d3d398;
-  margin-left: 2%;
-}
-</style> 
 
+.booked img {
+  width: 20px;
+  height: 20px;
+  border-radius: 5px;
+  margin-top: 1%;
+}
+
+
+.play {
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  align-items: center;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  padding: 6px 10px;
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 50px;
+  margin-left: 12%;
+  margin-top: -7%;
+  align-items: center;
+  display: none;
+
+
+}
+
+.play-container:hover+.play {
+  display: block;
+}
+
+.play img {
+  width: 35px;
+  height: 35px;
+  margin-left: 0%;
+  margin-top: 1%;
+}
+
+
+.play span {
+  color: whitesmoke;
+  margin-left: 5%;
+  margin-top: 5%;
+}
+
+/* Media Query for Tablets Ipads portrait mode */
+@media (min-width: 768px) and (max-width: 1024px) {
+  .recommended {
+    grid-template-columns: repeat(2, 1fr);
+
+  }
+
+  .booked {
+    margin-left: 38%;
+    margin-top: -190px;
+
+  }
+
+  .catImg {
+    margin-left: 63px;
+  }
+}
+
+/* Media Query for Mobile Devices */
+@media (max-width: 480px) {
+  .recommended {
+    grid-template-columns: repeat(1, 1fr);
+
+  }
+
+  .booked {
+    margin-left: 75%;
+    margin-top: -190px;
+
+  }
+
+  .catImg {
+    margin-left: 60px;
+  }
+}
+
+/* Media Query for low resolution  Tablets, Ipads */
+@media (min-width: 481px) and (max-width: 767px) {
+  .recommended {
+    grid-template-columns: repeat(2, 1fr);
+    width: 90%;
+
+  }
+
+  .booked {
+    margin-left: 75%;
+    margin-top: -190px;
+
+  }
+
+  .catImg {
+    margin-left: 60px;
+  }
+}
+</style>
